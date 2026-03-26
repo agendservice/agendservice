aaed5fbbaeb172f05b7a44b6c21f16bce5f73860
Fernandes Júnior <ter10365@mdb.com.br>
2026-03-26 09:54:17 -0300

    Adição do método login no UsuarioController

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -7,9 +7,26 @@ namespace App\Http\Controllers;
 use App\Http\Resources\UsuarioResource;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsuariosController extends Controller
 {
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $usuario = Usuario::whereRaw('upper(email) = upper(?)', [$request->email])->first();
+            Auth::login($usuario);
+            $user = Auth::user();
+
+            return response()->json(['redirect' => '/dashboard']);
+        }
+
+        return response()->json([
+            'code' => 500,
+            'message' => 'Falha na Autenticação.',
+        ], 500);
+    }
+
     public function index()
     {
         return UsuarioResource::collection(Usuario::all());
